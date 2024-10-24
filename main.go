c96afbd95498e95b75f5670caf2e043a34d1f7dd
Daniel R <nzdanron@gmail.com>
2024-10-24 17:12:14 +1300

    feat: add ascii startup banner
    Added a cool little banner for displaying the port the server is being
    hosted on

--- main.go
+++ main.go
@@ -15,6 +15,33 @@ func main() {
 	healthHandler := handlers.HealthHandler{}
 	mux.HandleFunc("GET /health", healthHandler.HandleGetHealth)
 
-	fmt.Printf("Serving on port %v", port)
+	printAsciiBanner(port)
 	http.ListenAndServe(fmt.Sprintf(":%v", port), mux)
+}
+
+// Prints an ascii banner to the terminal. Used before server startup
+func printAsciiBanner(port int) {
+	// ASCII art from: https://www.asciiart.eu/sports-and-outdoors/chess
+	asciiBanner := []string{
+		`   ^^__                             _                     `, // 0
+		`  /  - \_                          | |                    `, // 1
+		`<|    __<    ____ ___  _ __    ___ | |__    ___  ___  ___ `, // 2
+		`<|    \     |_  // _ \| '_ \  / __|| '_ \  / _ \/ __|/ __|`, // 3
+		`<|     \     / /|  __/| | | || (__ | | | ||  __/\__ \\__ \`, // 4
+		`<|______\   /___|\___||_| |_| \___||_| |_| \___||___/|___/`, // 5
+		` _|____|_                                                 `, // 6
+		`(________)               Serving on port %v`,                // 7
+		`/________\                                                `, // 8
+		`                                                          `, // 8
+	}
+
+	for i, line := range asciiBanner {
+		if i == 7 {
+			lineWithPort := fmt.Sprintf(line, port)
+			fmt.Println(lineWithPort)
+			continue
+		}
+
+		fmt.Println(line)
+	}
 }
