5f8bbebc2d62e0a2c721b6a6e5d2f287c3b5f363
Daniel R <nzdanron@gmail.com>
2024-11-02 00:46:33 +1300

    feat: added some styling to the board
    Got tired of looking at an ugly board for so long

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -4,8 +4,8 @@ import Chessboard from "./Chessboard";
 const App = {
   view: () => {
     return m("div", {
-      class: "h-screen w-screen flex justify-center items-center"
-    }, m(Chessboard))
+      class: "h-screen w-screen flex justify-center items-center bg-gradient-to-r from-blue-200 to-cyan-200"
+    }, m(Chessboard, { size: 90 * 8 }))
   }
 };
 
--- frontend/src/views/Chessboard.js
+++ frontend/src/views/Chessboard.js
@@ -1,6 +1,12 @@
 import m from "mithril";
 import ChessPiece from "./ChessPiece";
 
+/**
+ * Figures out what colour the square given at coordinates j,i should be
+ *
+ * @param {number} i The row the square is on
+ * @param {number} j The column the square is in
+ **/
 const calculateSquareColour = (i, j) => {
   const divByTwo = (x) => x % 2 == 0;
 
@@ -14,7 +20,19 @@ const calculateSquareColour = (i, j) => {
   return divByTwo(j) ? darkColour : lightColour
 };
 
-const squareSize = 90;
+/**
+ * Figures out what rounding should be applied to the current square, if any
+ *
+ * @param {number} i The row the square is on
+ * @param {number} j The column the square is in
+ **/
+const calculateSquareRounding = (i, j) => {
+  if (i == 0 && j == 0) return " rounded-tl-xl";
+  if (i == 0 && j == 7) return " rounded-tr-xl";
+  if (i == 7 && j == 0) return " rounded-bl-xl";
+  if (i == 7 && j == 7) return " rounded-br-xl";
+  return "";
+}
 
 const Chessboard = {
   board: [
@@ -27,14 +45,18 @@ const Chessboard = {
     [6, 6, 6, 6, 6, 6, 6, 6],
     [5, 4, 3, 2, 1, 3, 4, 5],
   ],
-  view: () => {
+  view: (vn) => {
+    const squareSize = vn.attrs.size / 8;
+
     return m("div", {
-      class: "flex flex-col gap-0 shadow"
+      class: "flex flex-col gap-0 rounded-xl shadow-2xl"
     }, Chessboard.board.map((row, i) => m("div", {
       class: "flex flex-row gap-0"
     }, row.map((piece, j) => {
       const colour = calculateSquareColour(i,j);
-      return m("div." + colour, {
+      const rounding = calculateSquareRounding(i,j);
+      
+      return m("div." + colour + rounding, {
         style: "width: " + squareSize + "px; height: " + squareSize + "px;"
       }, m(ChessPiece, { piece }))
     }))))
