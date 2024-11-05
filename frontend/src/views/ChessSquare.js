fc05426e127629d401e95c631e1e9553ac26e2ac
Daniel R <nzdanron@gmail.com>
2024-11-06 00:33:00 +1300

    fix: move highlighting no longer leaves phantom piece

--- frontend/src/views/ChessSquare.js
+++ frontend/src/views/ChessSquare.js
@@ -3,16 +3,16 @@ import { getPieceString } from "../utils";
 
 const ChessSquare = {
   view: (vn) => {
-    if (vn.attrs.pieceId == 0 && !vn.attrs.isSelected && !vn.attrs.isAvailableMove) return;
-
     let child = "";
-    if (vn.attrs.isSelected) {
-      child = m("div", { class: "z-100 relative top-0 left-0 border-4 border-red-500 w-full h-full rounded" })
-    }
     if (vn.attrs.isAvailableMove) {
       child = m("div", { class: "z-100 relative top-0 left-0 border-4 border-blue-500 w-full h-full rounded" })
       if (vn.attrs.pieceId == 0) return child; // If there is no piece, don't render one
     }
+    if (vn.attrs.isSelected) {
+      child = m("div", { class: "z-100 relative top-0 left-0 border-4 border-red-500 w-full h-full rounded" })
+    }
+
+    if (vn.attrs.pieceId == 0) return;
 
     return m("div.chesspiece .z-4 ." + getPieceString(vn.attrs.pieceId), child)
   }
