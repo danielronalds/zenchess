762c4fc56af030e9d2193e1d720cbbb2813f5e80
Daniel R <nzdanron@gmail.com>
2024-11-08 16:23:46 +1300

    refactor: reorganise bishop, rook, and knight code

--- frontend/src/movementCalculation.js
+++ frontend/src/movementCalculation.js
@@ -6,7 +6,20 @@ import { getPieceString, isPlayerPiece } from "./utils";
  * @param {number} y
  * @returns {boolean} True if the square is empty, or an enemy piece occupies it
  */
-const isValidSquare = (board, x, y, isWhite) => board[y][x] === 0 || isEnemyPiece(board, x, y, isWhite)
+const isValidSquare = (board, x, y, isWhite) => isEmptySquare(board, x, y) || isEnemyPiece(board, x, y, isWhite)
+
+/**
+ * Determines if the given coords are in bounds
+ */
+const isInBoard = (board, x, y) => x >= 0 && x < board.length && y >= 0 && y < board.length;
+
+/**
+  * Determines if a given square is empty 
+  * @param {number} x
+  * @param {number} y
+  * @returns {boolean} True if the square is empty
+  */
+const isEmptySquare = (board, x, y) => isInBoard(board, x, y) && board[y][x] === 0
 
 /**
  * Determines if there is an enemy piece at the given square
@@ -14,8 +27,9 @@ const isValidSquare = (board, x, y, isWhite) => board[y][x] === 0 || isEnemyPiec
  * @param {number} y
  * @returns {boolean} True if the given square contains an enemy piece
  */
-const isEnemyPiece = (board, x, y, isWhite) => (isWhite && board[y][x] > 6)
-                                 || (!isWhite && board[y][x] < 7 && board[y][x] !== 0)
+const isEnemyPiece = (board, x, y, isWhite) => isInBoard(board, x, y) 
+  && ((isWhite && board[y][x] > 6)
+  || (!isWhite && board[y][x] < 7 && board[y][x] !== 0));
 
 
 const calculateAvailableSquaresPawn = (board, x, y, isWhite) => {
@@ -73,12 +87,7 @@ const calculateAvailableSquaresPawn = (board, x, y, isWhite) => {
   return isWhite ? availableSquaresWhitePawn() : availableSqauresBlackPawn();
 }
 
-// FIXME: this could be refactored to be wayyyy nicer
 const calculateAvailableSquaresKnight = (board, x, y, isWhite) => {
-  const possibleMoves = [];
-
-  const chessboardSize = 8;
-
   // Creating the cross, then filtering options based on whether the square is available
   //
   // 0  x x
@@ -86,63 +95,46 @@ const calculateAvailableSquaresKnight = (board, x, y, isWhite) => {
   // 2   k
   // 3 x   x
   // 4  x x
+  const crossSquares = [
+    { x: x - 1, y: y - 2 },
+    { x: x + 1, y: y - 2 },
+    { x: x - 2, y: y - 1 },
+    { x: x + 2, y: y - 1 },
+    { x: x - 2, y: y + 1 },
+    { x: x + 2, y: y + 1 },
+    { x: x - 1, y: y + 2 },
+    { x: x + 1, y: y + 2 },
+  ];
 
-  if (y - 2 >= 0) {
-    if (x - 1 >= 0) possibleMoves.push({ x: x - 1, y: y - 2 })
-    if (x + 1 < chessboardSize) possibleMoves.push({ x: x + 1, y: y - 2 })
-  }
-
-  if (y - 1 >= 0) {
-    if (x - 2 >= 0) possibleMoves.push({ x: x - 2, y: y - 1 })
-    if (x + 2 < chessboardSize) possibleMoves.push({ x: x + 2, y: y - 1 })
-  }
-
-  if (y + 1 < chessboardSize) {
-    if (x - 2 >= 0) possibleMoves.push({ x: x - 2, y: y + 1 })
-    if (x + 2 < chessboardSize) possibleMoves.push({ x: x + 2, y: y + 1 })
-  }
-
-  if (y + 2 < chessboardSize) {
-    if (x - 1 >= 0) possibleMoves.push({ x: x - 1, y: y + 2 })
-    if (x + 1 < chessboardSize) possibleMoves.push({ x: x + 1, y: y + 2 })
-  }
-
-  return possibleMoves.filter(p => {
-    square = board[p.y][p.x];
-    if (isWhite) {
-      return square == 0 || square > 6; // > 6 is black pieces
-    }
-
-    return square < 7; // < 7 is white pieces
-  });
+  return crossSquares.filter(p => isValidSquare(board, p.x, p.y, isWhite));
 }
 
 const calculateAvailableSquaresRook = (board, x, y, isWhite) => {
   const availableSquares = [];
 
   leftX = x - 1;
-  while (leftX >= 0 && isValidSquare(board, leftX, y, isWhite)) {
+  while (isValidSquare(board, leftX, y, isWhite)) {
     availableSquares.push({ x: leftX, y })
     if (isEnemyPiece(board, leftX, y, isWhite)) break;
     leftX -= 1;
   }
 
   rightX = x + 1;
-  while (rightX < board.length && isValidSquare(board, rightX, y, isWhite)) {
+  while (isValidSquare(board, rightX, y, isWhite)) {
     availableSquares.push({ x: rightX, y })
     if (isEnemyPiece(board, rightX, y)) break;
     rightX += 1;
   }
 
   upY = y - 1;
-  while (upY >= 0 && isValidSquare(board, x, upY, isWhite)) {
+  while (isValidSquare(board, x, upY, isWhite)) {
     availableSquares.push({ x, y: upY })
     if (isEnemyPiece(board, x, upY, isWhite)) break;
     upY -= 1;
   }
 
   downY = y + 1;
-  while (downY < board.length && isValidSquare(board, x, downY, isWhite)) {
+  while (isValidSquare(board, x, downY, isWhite)) {
     availableSquares.push({ x, y: downY })
     if (isEnemyPiece(board, x, downY, isWhite)) break;
     downY += 1;
@@ -161,20 +153,17 @@ const calculateAvailableSquaresBishop = (board, x, y, isWhite) => {
 
   topLeftX = x - 1;
   topLeftY = y - 1;
-  while (topLeftX >= 0 && topLeftY >= 0 && isValidSquare(board, topLeftX, topLeftY, isWhite)) {
-    availableSquares.push({x: topLeftX, y: topLeftY});
+  while (isValidSquare(board, topLeftX, topLeftY, isWhite)) {
+    availableSquares.push({ x: topLeftX, y: topLeftY });
     if (isEnemyPiece(board, topLeftX, topLeftY, isWhite)) break;
     topLeftX -= 1;
     topLeftY -= 1;
   }
 
-  // Shorthand constant for board length
-  const L = board.length;
-
   topRightX = x + 1;
   topRightY = y - 1;
-  while (topRightX < L && topRightY >= 0 && isValidSquare(board, topRightX, topRightY, isWhite)) {
-    availableSquares.push({x: topRightX, y: topRightY});
+  while (isValidSquare(board, topRightX, topRightY, isWhite)) {
+    availableSquares.push({ x: topRightX, y: topRightY });
     if (isEnemyPiece(board, topRightX, topRightY, isWhite)) break;
     topRightX += 1;
     topRightY -= 1;
@@ -182,8 +171,8 @@ const calculateAvailableSquaresBishop = (board, x, y, isWhite) => {
 
   bottomRightX = x + 1;
   bottomRightY = y + 1;
-  while (bottomRightX < L && bottomRightY < L && isValidSquare(board, bottomRightX, bottomRightY, isWhite)) {
-    availableSquares.push({x: bottomRightX, y: bottomRightY});
+  while (isValidSquare(board, bottomRightX, bottomRightY, isWhite)) {
+    availableSquares.push({ x: bottomRightX, y: bottomRightY });
     if (isEnemyPiece(board, bottomRightX, bottomRightY, isWhite)) break;
     bottomRightX += 1;
     bottomRightY += 1;
@@ -191,8 +180,8 @@ const calculateAvailableSquaresBishop = (board, x, y, isWhite) => {
 
   bottomLeftX = x - 1;
   bottomLeftY = y + 1;
-  while (bottomLeftX >= 0 && bottomLeftY < L && isValidSquare(board, bottomLeftX, bottomLeftY, isWhite)) {
-    availableSquares.push({x: bottomLeftX, y: bottomLeftY});
+  while (isValidSquare(board, bottomLeftX, bottomLeftY, isWhite)) {
+    availableSquares.push({ x: bottomLeftX, y: bottomLeftY });
     if (isEnemyPiece(board, bottomLeftX, bottomLeftY, isWhite)) break;
     bottomLeftX -= 1;
     bottomLeftY += 1;
